3e8c6b2ab987dfde59fff50284b202f0e0073f54
bsiaotickchong <bsiaotickchong@gmail.com>
2018-12-08 15:43:31 -0500

    Working editable weights for AssignmentBox that update the database when clicked out of

--- src/main/java/gui/EditableTextField.java
+++ src/main/java/gui/EditableTextField.java
@@ -1,13 +1,20 @@
 package gui;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import javax.swing.*;
 import java.awt.event.FocusEvent;
 import java.awt.event.FocusListener;
+import java.sql.SQLException;
 
 public abstract class EditableTextField extends JTextField implements FocusListener {
 
+    private final static Logger LOG = LoggerFactory.getLogger(EditableTextField.class);
+
     public EditableTextField(String text) {
         super(text);
+        addFocusListener(this);
     }
 
     @Override
@@ -17,8 +24,13 @@ public abstract class EditableTextField extends JTextField implements FocusListe
 
     @Override
     public void focusLost(FocusEvent e) {
-        updateText();
+        try {
+            updateText(e.paramString());
+        } catch (SQLException s) {
+            LOG.error(s.getMessage());
+            s.printStackTrace();
+        }
     }
 
-    abstract void updateText();
+    abstract void updateText(String updatedText) throws SQLException;
 }
--- src/main/java/gui/EditableWeight.java
+++ src/main/java/gui/EditableWeight.java
@@ -2,10 +2,14 @@ package gui;
 
 import assignments.AssignmentMetaData;
 import org.jooq.grading_app.db.h2.tables.pojos.StudentType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.sql.SQLException;
 
 public class EditableWeight extends EditableTextField {
+
+    private final static Logger LOG = LoggerFactory.getLogger(EditableWeight.class);
 
     private AssignmentMetaData assignmentMetaData;
     private StudentType studentType;
@@ -14,10 +18,21 @@ public class EditableWeight extends EditableTextField {
                           StudentType studentType) throws SQLException {
         super(assignmentMetaData.getWeightForStudentType(studentType).getWeightPercent().toString());
         this.assignmentMetaData = assignmentMetaData;
+        this.studentType = studentType;
     }
 
     @Override
-    void updateText() {
+    void updateText(String updatedString) throws SQLException {
+        LOG.debug("Clicked out with value: {}", this.getText());
 
+        double updatedWeight;
+        if (this.getText().isEmpty()) {
+            updatedWeight = 0;
+            this.setText("0.0");
+        } else {
+            updatedWeight = Double.parseDouble(this.getText());
+        }
+
+        assignmentMetaData.setWeightForStudentType(studentType, updatedWeight);
     }
 }
